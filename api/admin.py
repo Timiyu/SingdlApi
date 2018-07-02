f1f03d1580c079b01854b2879e9116209149c38d
wanghuan <wanghuan0718@outlook.com>
2018-07-02 20:32:12 +0800

    注册admin模型

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from api.models import articles, moves
 # Register your models here.
+
+@admin.register(articles)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'pushdate')
+
+@admin.register(moves)
+class MoveAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'pushdate')
+
