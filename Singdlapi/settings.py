5b2748691d924a3b4fc565dcf010af5ec39b717a
wanghuan <wanghuan0718@outlook.com>
2018-07-04 21:08:25 +0800

    增加字段控制是否在新建时发布

--- Singdlapi/settings.py
+++ Singdlapi/settings.py
@@ -87,12 +87,12 @@ DATABASES = {
         'PASSWORD': 'fan123FAN',
     }
 }
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -135,41 +135,40 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#url映射
-
+# Media文件URL映射
 MEDIA_URL = '/media/'
-#设置静态文件路径为主目录下
+# 设置静态文件路径为主目录下
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-#tinymce
+# tinymce
 
 TINYMCE_JS_URL = os.path.join(STATIC_URL, "/tiny_mce/tiny_mce.js")
 
 TINYMCE_JS_ROOT = os.path.join(STATIC_ROOT, "/tiny_mce")
 
 TINYMCE_DEFAULT_CONFIG = {
-    # // General options
+    # General options
     'mode': 'textareas',
     'theme': "advanced",
     'plugins': "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave",
 
-    # // Theme  options
+    # Theme  options
     'theme_advanced_buttons1': "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,fullscreen,code",
     'theme_advanced_buttons2': "cut,copy,paste,pastetext,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,|,insertdate,inserttime,preview,|,forecolor,backcolor",
     'theme_advanced_buttons3': "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl",
     'theme_advanced_toolbar_location': "top",
-    'theme_advanced_toolbar_align': "left",
+    'theme_advanced_toolbar_align': "center",
     'theme_advanced_statusbar_location': "bottom",
     'theme_advanced_resizing': 'true',
 
-    # // content_css: "/css/style.css",
+    # content_css: "/css/style.css",
     'template_external_list_url': "lists/template_list.js",
     'external_link_list_url': "lists/link_list.js",
     'external_image_list_url': "lists/image_list.js",
     'media_external_list_url': "lists/media_list.js",
 
-# // Style formats
+    # Style formats
     'style_formats': [
     {'title': 'Bold text', 'inline': 'strong'},
     {'title': 'Red text', 'inline': 'span', 'styles': {'color': '#ff0000'}},
