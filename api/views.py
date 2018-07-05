5237c64493ea3f2202f38d35ac4551078b8f6a49
wanghuan <wanghuan0718@outlook.com>
2018-07-05 15:57:27 +0800

    添加CORS模块对跨域访问进行统一管理

--- api/views.py
+++ api/views.py
@@ -7,46 +7,53 @@ import json
 
 
 # Create your views here.
+# 返回文章列表
 def article_list(request):
     data = models.articles.objects.all()
+
     data = serializers.serialize('json', data, fields=('author', 'title', 'comment', 'article_coverimg'))
+
     data = json.dumps(json.loads(data), ensure_ascii=False)
+
     response = HttpResponse(data, content_type="application/json")
-    response["Access-Control-Allow-Origin"] = "*"
-    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
-    response["Access-Control-Max-Age"] = "1000"
-    response["Access-Control-Allow-Headers"] = "*"
+
     return response
 
+# 返回文章详情
 def article_detail(request):
+
     data = models.articles.objects.all()
-    data = serializers.serialize('json', data, fields=('author', 'titile', 'content'))
+
+    data = serializers.serialize('json', data, fields=('author', 'title', 'content'))
+
     data = json.dumps(json.loads(data), ensure_ascii=False)
+
     response = HttpResponse(data, content_type="application/json")
-    response["Access-Control-Allow-Origin"] = "*"
-    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
-    response["Access-Control-Max-Age"] = "1000"
-    response["Access-Control-Allow-Headers"] = "*"
+
     return response
 
+# 返回视频列表
 def movie_list(request):
+
     data = models.movies.objects.all()
+
     data = serializers.serialize('json', data)
+
     data = json.dumps(json.loads(data), ensure_ascii=False)
+
     response = HttpResponse(data, content_type="application/json")
-    response["Access-Control-Allow-Origin"] = "*"
-    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
-    response["Access-Control-Max-Age"] = "1000"
-    response["Access-Control-Allow-Headers"] = "*"
+
     return response
 
+# 返回视频详情
 def movie_detail(request):
+
     data = models.movies.objects.all()
+
     data = serializers.serialize('json', data)
+
     data = json.dumps(json.loads(data), ensure_ascii=False)
+
     response = HttpResponse(data, content_type="application/json")
-    response["Access-Control-Allow-Origin"] = "*"
-    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
-    response["Access-Control-Max-Age"] = "1000"
-    response["Access-Control-Allow-Headers"] = "*"
+
     return response
