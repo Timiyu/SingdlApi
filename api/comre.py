d94e6996474b7087f42883fa5a1db3534d1a7ffd
wanghuan <wanghuan0718@outlook.com>
2018-07-18 12:21:22 +0800

    增加姓名正则匹配

--- api/comre.py
+++ api/comre.py
@@ -18,3 +18,20 @@ def phonecheck(data):
     else:
 
         return False
+
+
+def namecheck(data):
+
+    #正则表达式pattern
+
+    pattern = r"[\u4E00-\u9FA5]{2,6}(?:·[\u4E00-\u9FA5]{2,5})*"
+
+    result = re.findall(pattern, data)
+
+    if result:
+
+        return True
+
+    else:
+
+        return False
